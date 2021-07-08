f388d933c3bc6fc53031aeee0667c23a75c94377
juancortelezzi <juanbicortelezzi@gmail.com>
2021-07-08 15:41:20 -0300

    removed unused things on init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,17 +1,3 @@
 require("wiz")
 
 vim.g.colors_name = "nord"
-
--- let g:tokyonight_style = "night"
--- let g:tokyonight_italic_functions = 1
--- let g:tokyonight_sidebars = [ "qf", "vista_kind", "terminal", "packer" ]
--- let g:tokyonight_transparent = "true"
--- colorscheme tokyonight
-
-
--- function! Wiki()
---     colorscheme gruvbox
---     set tw=60
---     set colorcolumn=61
--- endfunction
-
