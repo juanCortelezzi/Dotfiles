ea629966e576882869ab9449ae1922faefc5089d
juancortelezzi <juanbicortelezzi@gmail.com>
2021-07-21 01:30:11 -0300

    fixed python config

--- nvim/.config/nvim/ftplugin/python.lua
+++ nvim/.config/nvim/ftplugin/python.lua
@@ -2,7 +2,7 @@ local nvim_lsp = require("lspconfig")
 local wiz_lsp = require("wiz.lsp")
 
 nvim_lsp.pyright.setup({
-  capabilities = lsp_config.capabilities,
+  capabilities = wiz_lsp.capabilities,
   on_attatch = wiz_lsp.common_on_attatch
 })
 
