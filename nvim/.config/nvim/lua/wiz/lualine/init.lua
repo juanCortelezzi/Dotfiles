40e1ab6a2b34370108f634ddf70af56503c04a05
juancortelezzi <juanbicortelezzi@gmail.com>
2021-10-28 13:45:31 -0300

    updated lualine

--- nvim/.config/nvim/lua/wiz/lualine/init.lua
+++ nvim/.config/nvim/lua/wiz/lualine/init.lua
@@ -2,17 +2,22 @@ require("lualine").setup({
   options = {
     icons_enabled = true,
     theme = "nord",
-    component_separators = { "", "" },
-    section_separators = { "", "" },
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
     disabled_filetypes = { "toggleterm", "startify", "alpha" },
+    always_divide_middle = false,
   },
   sections = {
     lualine_a = { "mode" },
-    lualine_b = { "branch" },
+    lualine_b = { "branch", { "diagnostics", sources = { "nvim_lsp" } } },
     lualine_c = { "filename" },
+
     lualine_x = { "filetype" },
     lualine_y = { "progress" },
     lualine_z = { "location" },
+    -- lualine_x = {},
+    -- lualine_y = {},
+    -- lualine_z = {},
   },
   inactive_sections = {
     lualine_a = {},
