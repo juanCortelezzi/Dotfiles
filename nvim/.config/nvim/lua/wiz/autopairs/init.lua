e62b0a44b83974ebf052b8a4ac58546b456256c8
juancortelezzi <juanbicortelezzi@gmail.com>
2021-10-28 13:45:24 -0300

    changed autopairs-cmp trigger

--- nvim/.config/nvim/lua/wiz/autopairs/init.lua
+++ nvim/.config/nvim/lua/wiz/autopairs/init.lua
@@ -19,8 +19,5 @@ npairs.add_rules({
   Rule("$", "$", "lua"):with_pair(ts_conds.is_not_ts_node({ "function" })),
 })
 
-require("nvim-autopairs.completion.cmp").setup({
-  map_cr = true, --  map <CR> on insert mode
-  map_complete = true, -- it will auto insert `(` after select function or method item
-  auto_select = true, -- automatically select the first item
-})
+-- add cmp <CR> mapping
+require("cmp").event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
