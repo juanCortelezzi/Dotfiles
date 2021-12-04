0e944b1a8cc16731f8682f94c14c2ae5fea37bd1
juancortelezzi <juanbicortelezzi@gmail.com>
2021-12-04 03:50:55 -0300

    updated telescope config

--- nvim/.config/nvim/lua/wiz/telescope/init.lua
+++ nvim/.config/nvim/lua/wiz/telescope/init.lua
@@ -1,8 +1,31 @@
 local actions = require("telescope.actions")
+local previewers = require("telescope.previewers")
 
 require("nvim-web-devicons").setup({
   default = true,
 })
+
+local Job = require("plenary.job")
+local new_maker = function(filepath, bufnr, opts)
+  filepath = vim.fn.expand(filepath)
+  Job
+    :new({
+      command = "file",
+      args = { "--mime-type", "-b", filepath },
+      on_exit = function(j)
+        local mime_type = vim.split(j:result()[1], "/")[1]
+        if mime_type == "text" then
+          previewers.buffer_previewer_maker(filepath, bufnr, opts)
+        else
+          -- maybe we want to write something to the buffer here
+          vim.schedule(function()
+            vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY" })
+          end)
+        end
+      end,
+    })
+    :sync()
+end
 
 require("telescope").setup({
   defaults = {
@@ -11,46 +34,19 @@ require("telescope").setup({
         ["<esc>"] = actions.close,
       },
     },
-    vimgrep_arguments = {
-      "rg",
-      "--color=never",
-      "--no-heading",
-      "--with-filename",
-      "--line-number",
-      "--column",
-      "--smart-case",
-    },
-    prompt_prefix = "> ",
-    selection_caret = "> ",
-    entry_prefix = "  ",
-    initial_mode = "insert",
-    selection_strategy = "reset",
-    sorting_strategy = "descending",
-    layout_strategy = "horizontal",
-    layout_config = {
-      horizontal = {
-        mirror = false,
-      },
-      vertical = {
-        mirror = false,
-      },
-    },
-    file_sorter = require("telescope.sorters").get_fuzzy_file,
     file_ignore_patterns = { "node_modules/.*", "target/.*", "bin/.*", ".next/.*", ".git/.*" },
-    generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-    winblend = 0,
-    border = {},
-    borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-    color_devicons = false,
-    use_less = true,
-    set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
-    file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-    grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-    qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-
-    -- Developer configurations: Not meant for general override
-    buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+    buffer_previewer_maker = new_maker,
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true, -- false will only do exact matching
+      override_generic_sorter = true, -- override the generic sorter
+      override_file_sorter = true, -- override the file sorter
+      case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+      -- the default case_mode is "smart_case"
+    },
   },
 })
 
 require("telescope").load_extension("projects")
+require("telescope").load_extension("fzf")
