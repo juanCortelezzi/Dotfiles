6c24771694da1935fa479aaa8cbf971b8b4770fa
juancortelezzi <juanbicortelezzi@gmail.com>
2021-12-04 03:50:36 -0300

    moved vimwiki to lua

---
+++ nvim/.config/nvim/lua/wiz/vimwiki/init.lua
@@ -0,0 +1,15 @@
+vim.g.vimwiki_list = {
+  {
+    path = "~/Documents/Stuff/Notas/Vim_wiki/",
+    ext = ".wiki",
+    template_path = "~/Documents/Stuff/Notas/Vim_wiki_templates/",
+    template_default = "custom_template",
+    template_ext = ".html",
+    path_html = "~/Documents/Stuff/Notas/Vim_wiki_HTML/",
+    auto_export = 0,
+    links_space_char = "_",
+    css_name = "Styles/custom_style.css",
+  },
+}
+
+vim.g.vimwiki_listsyms = " ○◐●✓"
