10fdf5a2eb447edda61bf33a2f3de1bc4d803109
juancortelezzi <juanbicortelezzi@gmail.com>
2021-12-11 12:31:36 -0300

    nvim disable default pluggins ?

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -16,3 +16,13 @@ vim.cmd([[autocmd BufNewFile,BufRead *.wiki,*.mdx set ft=markdown]])
 -- bundle whichkey mappings on config function plugin load
 require("wiz.plugins")
 require("wiz.mappings")
+-- vim.g.loaded_gzip         = 1
+-- vim.g.loaded_tar          = 1
+-- vim.g.loaded_tarPlugin    = 1
+-- vim.g.loaded_zipPlugin    = 1
+-- vim.g.loaded_2html_plugin = 1
+-- vim.g.loaded_netrw        = 1
+-- vim.g.loaded_netrwPlugin  = 1
+-- vim.g.loaded_matchit      = 1
+-- vim.g.loaded_matchparen   = 1
+-- vim.g.loaded_spec         = 1
