eb606a128a763c80e74e7fadc19ee9f6f92fbb72
J.B.C <juanbicortelezzi@gmail.com>
2022-05-05 17:19:20 -0300

    nvim update nvimtree

--- nvim/.config/nvim/lua/wiz/nvimtree.lua
+++ nvim/.config/nvim/lua/wiz/nvimtree.lua
@@ -9,14 +9,9 @@ nvimtree.setup({
   hijack_netrw = true,
   open_on_setup = false,
   ignore_ft_on_setup = {},
-  auto_close = false,
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = false,
-  update_to_buf_dir = {
-    enable = true,
-    auto_open = true,
-  },
   diagnostics = {
     enable = false,
     icons = {
@@ -49,7 +44,6 @@ nvimtree.setup({
     height = 30,
     hide_root_folder = false,
     side = "right",
-    auto_resize = false,
     mappings = {
       custom_only = false,
       list = {},
