8b4f912f02994d83bb7660cb169a07933e3bc153
J.B.C <juanbicortelezzi@gmail.com>
2023-01-07 22:14:51 +1300

    fix(nvim) toggleterm now loads on keybind

--- nvim/.config/nvim/lua/plugins/toggleterm/init.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm/init.lua
@@ -1,5 +1,6 @@
 return {
   "akinsho/toggleterm.nvim",
+  keys = [[<C-\>]],
   config = {
     -- size can be a number or function which is passed the current terminal
     size = function(term)
@@ -12,7 +13,7 @@ return {
     open_mapping = [[<C-\>]],
     hide_numbers = true, -- hide the number column in toggleterm buffers
     shade_filetypes = {},
-    shade_terminals = false,
+    shade_terminals = true,
     shading_factor = "1", -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
     start_in_insert = true,
     insert_mappings = true, -- whether or not the open mapping applies in insert mode
