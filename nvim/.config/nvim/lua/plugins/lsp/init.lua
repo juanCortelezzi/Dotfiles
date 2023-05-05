1b0c1ba35ef6466757fa963cd53ded055dd293b9
J.B.C <juanbicortelezzi@gmail.com>
2023-05-05 12:29:53 +1200

    fix(nvim) move null-ls config to lsp config

--- nvim/.config/nvim/lua/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/plugins/lsp/init.lua
@@ -4,6 +4,15 @@ local M = {
   dependencies = {
     "simrat39/rust-tools.nvim",
     "folke/neodev.nvim",
+    {
+      "jose-elias-alvarez/null-ls.nvim",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      --   has_formatter = function(ft)
+      --     local sources = require("null-ls.sources")
+      --     local available = sources.get_available(ft, "NULL_LS_FORMATTING")
+      --     return #available > 0
+      --   end,
+    },
   },
 }
 
@@ -11,8 +20,14 @@ function M.config()
   local lspconfig = require("lspconfig")
   local mason_lsp = require("mason-lspconfig")
   local handlers = require("plugins.lsp.config")
+  local null_ls = require("null-ls")
   local on_attach = handlers.on_attach
   local capabilities = handlers.capabilities
+  -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+  local formatting = null_ls.builtins.formatting
+
+  -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+  -- local diagnostics = null_ls.builtins.diagnostics
 
   mason_lsp.setup_handlers({
     -- The first entry (without a key) will be the default handler
@@ -79,12 +94,64 @@ function M.config()
       lspconfig.tailwindcss.setup({
         on_attach = on_attach,
         capabilities = capabilities,
-        filetypes = { "typescript", "typescriptreact", "typescript.tsx", "astro", "javascript" },
+        filetypes = {
+          "typescript",
+          "typescriptreact",
+          "typescript.tsx",
+          "astro",
+          "javascript",
+        },
       })
     end,
   })
 
-  require("plugins.null-ls").setup(on_attach)
+  null_ls.setup({
+    debug = false,
+    debounce = 150,
+    on_attach = on_attach,
+    sources = {
+      -- js, ts, json
+      formatting.prettier.with({
+        extra_filetypes = { "toml", "astro" },
+      }),
+      -- python
+      formatting.black.with({ extra_args = { "--fast" } }),
+      -- diagnostics.flake8,
+      -- lua
+      formatting.stylua.with({
+        extra_args = {
+          "--config-path",
+          vim.fn.stdpath("config") .. "/stylua.toml",
+        },
+      }),
+      -- golang
+      -- formatting.gofmt,
+      -- formatting.gofumpt.with({
+      --   extra_args = { "--extra" },
+      --   env = {
+      --     GOFUMPT_SPLIT_LONG_LINES = "on",
+      --   },
+      -- }),
+      formatting.golines.with({
+        extra_args = { "-m", 80, "-t", 2, "--base-formatter", "gofumpt" },
+        -- command = 'golines %s -w --base-formatter="gofumpt"',
+      }),
+
+      -- rust
+      formatting.rustfmt.with({
+        extra_args = { "--edition", 2021 },
+      }),
+      -- bash
+      null_ls.builtins.diagnostics.shellcheck,
+      formatting.beautysh.with({
+        extra_args = { "--indent-size", 2 },
+      }),
+      -- zig
+      formatting.zigfmt,
+      -- yaml
+      formatting.yamlfmt,
+    },
+  })
 end
 
 function M.init()
