0764141222deb67beb69c0e67f74bc0ca7ad180c
J.B.C <juanbicortelezzi@gmail.com>
2023-08-30 21:49:34 +1200

    feat(nvim) add copilot

---
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,10 @@
+return {
+  "github/copilot.vim",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  -- init = function()
+  --   vim.g.copilot_filetypes = {
+  --     ["*"] = 0,
+  --   }
+  -- end,
+}
