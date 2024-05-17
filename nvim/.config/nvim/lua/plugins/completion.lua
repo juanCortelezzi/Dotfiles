8759f543dfc22aac89f87380e1dc6493775ab795
J.B.C <juanbicortelezzi@gmail.com>
2024-05-17 17:04:31 +1200

    feat(nvim): rework completion

---
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,130 @@
+---@type LazySpec
+return {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "nvim-autopairs",
+    "onsails/lspkind.nvim",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    {
+      "L3MON4D3/LuaSnip",
+      dependencies = {
+        { "rafamadriz/friendly-snippets" },
+        { "saadparwaiz1/cmp_luasnip" },
+      },
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+      end,
+    },
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    local lspkind = require("lspkind")
+
+    luasnip.config.set_config({
+      history = false,
+      updateevents = "TextChanged,TextChangedI",
+    })
+
+    cmp.setup({
+      preselect = cmp.PreselectMode.None,
+
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+      }, { name = "nvim_lua" }, {
+        { name = "buffer", keyword_length = 5, max_item_count = 3 },
+        { name = "path" },
+      }),
+
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+
+      completion = {
+        completeopt = "menu,menuone,noinsert,noselect",
+      },
+
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+
+      mapping = cmp.mapping.preset.insert({
+        ["<C-n>"] = cmp.mapping.select_next_item({
+          behavior = cmp.SelectBehavior.Insert,
+        }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({
+          behavior = cmp.SelectBehavior.Insert,
+        }),
+        ["<C-d>"] = cmp.mapping.scroll_docs(2),
+        ["<C-u>"] = cmp.mapping.scroll_docs(-2),
+        ["<C-j>"] = cmp.mapping(function(fallback)
+          if luasnip.locally_jumpable() then
+            luasnip.jump(1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<C-k>"] = cmp.mapping(function(fallback)
+          if luasnip.locally_jumpable() then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<C-y>"] = cmp.mapping(
+          cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true,
+          }),
+          { "i", "c" }
+        ),
+        -- -- Accept currently selected item. If none selected, `select` first item.
+        -- -- Set `select` to `false` to only confirm explicitly selected items.
+        -- ["<CR>"] = cmp.mapping.confirm({
+        --   select = false,
+        --   behavior = cmp.ConfirmBehavior.Replace,
+        -- }),
+      }),
+      formatting = {
+        -- fields = { "kind", "abbr", "menu" },
+        format = lspkind.cmp_format({
+          mode = "symbol",
+          maxwidth = 50,
+          ellipsis_char = "...",
+          show_labelDetails = true,
+        }),
+      },
+    })
+
+    -- `/` cmdline setup.
+    cmp.setup.cmdline("/", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" },
+      },
+    })
+
+    -- `:` cmdline setup.
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+      }, {
+        { name = "cmdline" },
+      }),
+    })
+
+    cmp.event:on(
+      "confirm_done",
+      require("nvim-autopairs.completion.cmp").on_confirm_done()
+    )
+  end,
+}
