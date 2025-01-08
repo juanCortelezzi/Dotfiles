e9777faecafc708ac5779e40ed3a60a50dfa879c
J.B.C <juanbicortelezzi@gmail.com>
2025-01-08 19:48:19 +1300

    fix: formatting config

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -3,37 +3,33 @@ return {
   "stevearc/conform.nvim",
   event = { "BufWritePre" },
   cmd = { "ConformInfo" },
-  ---@module "conform"
-  ---@param opts conform.setupOpts
-  opts = function(_, opts)
-    local util = require("conform.util")
+  opts = function()
+    local opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        go = { "gofmt" },
+        templ = { "gofmt" },
+        rust = { "rustfmt" },
+        bash = { "shfmt" },
+        zig = { "zigfmt" },
+        ocaml = { "ocamlformat" },
+      },
+      default_format_opts = {
+        timeout_ms = 3000,
+        async = false, -- not recommended to change
+        quiet = false, -- not recommended to change
+        lsp_format = "fallback", -- not recommended to change
+      },
 
-    opts.formatters_by_ft = {
-      lua = { "stylua" },
-      python = { "isort", "black" },
-      go = { "gofmt" },
-      templ = { "gofmt" },
-      rust = { "rustfmt" },
-      bash = { "shfmt" },
-      zig = { "zigfmt" },
-      ocaml = { "ocamlformat" },
-    }
-
-    opts.default_format_opts = {
-      timeout_ms = 3000,
-      async = false, -- not recommended to change
-      quiet = false, -- not recommended to change
-      lsp_format = "fallback", -- not recommended to change
-    }
-
-    -- enable format on save
-    opts.format_on_save = {}
-
-    opts.formatters = {
-      -- LazyVim has this so I have to have it too.
-      injected = { options = { ignore_errors = true } },
-      shfmt = {
-        prepend_args = { "-i", "2" },
+      -- enable format on save
+      format_on_save = {},
+      formatters = {
+        -- LazyVim has this so I have to have it too.
+        injected = { options = { ignore_errors = true } },
+        shfmt = {
+          prepend_args = { "-i", "2" },
+        },
       },
     }
 
@@ -63,6 +59,8 @@ return {
         stop_after_first = true,
       }
     end
+
+    return opts
   end,
   init = function()
     -- If you want the formatexpr, here is the place to set it
